e35051e0bad07d7887d7fe992d24f0791d63be67
Noskcaj <kcajdev@gmail.com>
2017-10-14 16:41:13 -0400

    Modified autogear values and added niVision

--- src/org/usfirst/frc/team1523/robot/commands/AutoGear.java
+++ src/org/usfirst/frc/team1523/robot/commands/AutoGear.java
@@ -8,43 +8,44 @@ import edu.wpi.first.wpilibj.command.Command;
 public class AutoGear extends CommandGroup {
 
 	/*
-	 * 0 = Left no hopper
+	 * 0 = Left shoot
 	 * 1 = Center
-	 * 2 = Right no hopper
-	 * 3 = Left hopper
-	 * 4 = Right hopper
+	 * 2 = Right shoot
+	 * 3 = Left no shoot
+	 * 4 = Right no shoot
 	 */
 
 	public AutoGear(int pos) {
 		//Left
 		if (pos == 0 || pos == 3) {
 			Robot.drive.reset();
-			addSequential(new AutoDrive(0.2, 55));
+			addSequential(new AutoDrive(0.2, 55.5));
 			addSequential(new AutoTurn(-45));
-			addSequential(new AutoDrive(0.2, 2));
+			addSequential(new AutoDrive(0.1, 3));
 
 		}
 		//Center
 		else if (pos == 1) {
 			Robot.drive.reset(); //resets encoders
-			addSequential(new AutoDrive(0.15, 34));
+			addSequential(new AutoDrive(0.2, 45));
 
 		}
 		//Right
 		else if (pos == 2 || pos == 4) {
 			Robot.drive.reset();
-			addSequential(new AutoDrive(0.2, 55));
+			addSequential(new AutoDrive(0.2, 55.5));
 			addSequential(new AutoTurn(45));
-			addSequential(new AutoDrive(0.2, 2));
+			addSequential(new AutoDrive(0.1, 3));
+			
 		}
 		//Find Target
 		//		addSequential(new Vision());
 		//Put Gear on Peg
 		if (pos == 0 || pos == 1 || pos == 2) {
-			//Robot.drive.reset();     //resets encoders
+			Robot.drive.reset();     //resets encoders
 			//addSequential(new Robot.drive.reset());
 			addSequential(new SetUpperGear(false));
-			addSequential(new WaitCommand(2));
+			addSequential(new WaitCommand(4));
 			addSequential(new SetUpperGear(true));
 		}
 	}
